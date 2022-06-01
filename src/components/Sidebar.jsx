f918cb4cdd70170e62f1df6e7da2069fc1998207
topleftstyll <joshua.brackett@hotmail.ca>
2022-06-01 18:01:11 -0400

    implemented theme selected you can now change colors and pick dark or light theme

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,8 @@ const Sidebar = () => {
   const {
     activeMenu,
     setActiveMenu,
-    screenSize
+    screenSize,
+    currentColor
   } = useStateContext()
 
   const handleCloseSideBar = () => {
@@ -19,8 +20,8 @@ const Sidebar = () => {
     }
   }
 
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounged-lg text-white text-md m-2'
-  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounged-lg text-md text-gray-700 dark:text-gray-200 dark:text-black hover:bg-light-gray mt-2'
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:text-white hover:bg-light-gray mt-2'
 
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
@@ -28,7 +29,7 @@ const Sidebar = () => {
         <>
           <div className='flex justify-between items-center'>
             <Link to='/' onClick={handleCloseSideBar} 
-              className="items-center gap-3 ml-3 mt-4 flex text-xl font-exrabold tracking-tight dark:text-white text-slate-900"
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware />
               <span>Shoppy</span>
@@ -52,6 +53,13 @@ const Sidebar = () => {
                     to={`/${link.name}`}
                     key={link.name}
                     onClick={handleCloseSideBar}
+                    style={({ isActive }) => 
+                      ({
+                        backgroundColor: isActive 
+                          ? currentColor 
+                          : ''
+                      }) 
+                    }
                     className={({ isActive }) => isActive ? activeLink : normalLink}
                   >
                     {link.icon}
